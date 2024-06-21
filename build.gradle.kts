7150d36121917ec49770f98df7ffba693dc8d754
Kotler <kotler.developer@gmail.com>
2024-06-21 21:48:20 +0300

    qa-practice: hw1 init

---
+++ build.gradle.kts
@@ -0,0 +1,158 @@
+plugins {
+//    kotlin("jvm") version "1.4.30"
+    id("java")
+    id("com.adarshr.test-logger") version "2.1.1"
+    id("io.qameta.allure") version "2.8.1"
+    id("com.github.ben-manes.versions") version "0.36.0"
+}
+
+group = "dev.kotler"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    jcenter()
+}
+configurations {
+    create("testCompile")
+}
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+
+
+dependencies {
+    val codeborneSelenideVersion = "7.3.2"
+    val fluentleniumVersion = "4.6.1"
+    val seleniumJavaVersion = "4.22.0"
+    val webdriverManagerVersion = "5.8.0"
+    val browsermobVersion = "2.1.5"
+    val skrapeitVersion = "1.0.0-alpha8"
+    val jUnitVersion = "5.5.2"
+    val assertjVersion = "3.19.0"
+    val striktVersion = "0.28.2"
+    val awaitilityVersion = "4.0.3"
+    val rerunnerVersion = "2.1.6"
+    val kotlinLoggerVersion = "2.0.4"
+    val julToSlf4jVersion = "1.7.28"
+
+    testImplementation(
+        group = "com.codeborne",
+        name = "selenide",
+        version = codeborneSelenideVersion
+    )
+
+    testImplementation(
+        group = "org.seleniumhq.selenium",
+        name = "selenium-java",
+        version = seleniumJavaVersion
+    )
+    testImplementation(
+        group = "io.github.bonigarcia",
+        name = "webdrivermanager",
+        version = webdriverManagerVersion
+    )
+    testImplementation(
+        group = "net.lightbody.bmp",
+        name = "browsermob-proxy",
+        version = browsermobVersion
+    )
+    testImplementation(
+        group = "it.skrape",
+        name = "skrapeit-core",
+        version = skrapeitVersion
+    )
+
+    testImplementation(
+        group = "org.assertj",
+        name = "assertj-core",
+        version = assertjVersion
+    )
+    testImplementation(
+        group = "org.fluentlenium",
+        name = "fluentlenium-junit-jupiter",
+        version = fluentleniumVersion
+    )
+    testImplementation(
+        group = "org.fluentlenium",
+        name = "fluentlenium-assertj",
+        version = fluentleniumVersion
+    )
+    testImplementation(
+        group = "io.strikt",
+        name = "strikt-core",
+        version = striktVersion
+    )
+    testImplementation(
+        group = "org.junit.jupiter",
+        name = "junit-jupiter",
+        version = jUnitVersion
+    )
+    testImplementation(
+        group = "io.github.artsok",
+        name = "rerunner-jupiter",
+        version = rerunnerVersion
+    )
+    testImplementation(
+        group = "org.awaitility",
+        name = "awaitility-kotlin",
+        version = awaitilityVersion
+    )
+    testImplementation(
+        group = "io.github.microutils",
+        name = "kotlin-logging",
+        version = kotlinLoggerVersion
+    )
+    testImplementation(
+        group = "org.slf4j",
+        name = "jul-to-slf4j",
+        version = julToSlf4jVersion
+    )
+}
+
+configurations {
+    all {
+        exclude(module = "junit")
+        exclude(module = "htmlunit-driver")
+        exclude(module = "slf4j-log4j12")
+        exclude(module = "slf4j-simple")
+        exclude(module = "log4j")
+    }
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+        parallelTestExecution()
+
+        systemProperty("browser", System.getProperty("browser"))
+
+        finalizedBy("allureReport")
+    }
+
+
+}
+
+fun Test.parallelTestExecution() {
+    val parallel = "junit.jupiter.execution.parallel"
+    if (!project.hasProperty("serial")) {
+        systemProperties = mapOf(
+            "$parallel.enabled" to true,
+            "$parallel.mode.default" to "concurrent",
+            "$parallel.config.dynamic.factor" to 4
+        )
+    }
+}
+
+allure {
+    val allureVersion = "2.29.0"
+    version = allureVersion
+    autoconfigure = true
+    aspectjVersion = "1.9.6"
+    useJUnit5 {
+        version = allureVersion
+    }
+}
---
+++ src/test/java/AuthInQaSchoolTest.java
@@ -0,0 +1,96 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.devtools.CdpVersionFinder;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AuthInQaSchoolTest {
+//
+//    WebDriver driver;
+//
+//    @BeforeAll
+//    static void setupClass() {
+//        WebDriverManager.firefoxdriver().setup();
+//    }
+//
+//    @BeforeEach
+//    void setupTest() {
+//        driver = new FirefoxDriver();
+//    }
+//
+//    @AfterEach
+//    void teardown() {
+//        driver.quit();
+//    }
+//
+//    @Test
+//    void test() {
+//        // Exercise
+//        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+//        String title = driver.getTitle();
+//
+//        // Verify
+//        assertThat(title).contains("Selenium WebDriver");
+//    }
+
+    @Test
+    void authTest() throws InterruptedException {
+//        System.setProperty("webdriver.chrome.driver", "/home/kotler/.cache/selenium/chrome/linux64/114.0.5735.133/chrome");
+        Logger.getLogger(CdpVersionFinder.class.getName()).setLevel(Level.SEVERE);
+        System.setProperty("webdriver.chromium.driver", "/usr/lib64/chromium-browser");
+        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--no-sandbox");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        driver.get("https://school.qa.guru/cms/system/login");
+
+        Thread.sleep(1000);
+
+        if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+        driver.quit();
+    }
+
+    @Test
+    void checkLaunchFirefox() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.selenium.dev");
+        driver.getTitle();
+        driver.quit();
+    }
+
+    @Test
+    void checkLaunchChromium() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev");
+        driver.getTitle();
+        driver.quit();
+    }
+
+    @Test
+    void launchBrowser() throws InterruptedException {
+        checkLaunchFirefox();
+        open("https://www.selenium.dev");
+        Thread.sleep(2000);
+    }
+}
